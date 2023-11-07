9132ad821456a9b6ae196b9e919f8f7b751c3b4b
Dhruv Desai <dhruvdesai770@gmail.com>
2023-11-07 07:34:50 -0500

    Add to cart in product page -Done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect } from 'react'
 import { Badge, Button, Container, Nav, Navbar } from 'react-bootstrap'
 import { Link, Outlet } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { Store } from './Store'
 
 
@@ -19,10 +22,13 @@ function App() {
   }
   return (
     <div className='d-flex flex-column vh-100'>
+      <ToastContainer position="bottom-center" limit={1} />
       <header>
         <Navbar expand="lg">
           <Container>
-            <Navbar.Brand>Apnabazar</Navbar.Brand>
+          <LinkContainer to="/">
+              <Navbar.Brand>ApnaBazar</Navbar.Brand>
+            </LinkContainer>
           </Container>
           <Nav>
           <Button variant={mode} onClick={switchModeHandler}>
